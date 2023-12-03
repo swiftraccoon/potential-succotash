3f51734185f0aa14ec5c7698cbc3e1216cac7528
swiftraccoon <swiftraccoon-gh@wnc.icu>
2023-12-03 04:46:46 -0500

    Refactor webauthn.js for readability and logging
    Revamped `webauthn.js` to improve code readability by consistent formatting and adding explanatory comments. Enhanced error handling and diagnostics by incorporating additional logging for the authentication processes and challenges. Adapted credential ID handling in the verification process to be compatible with binary data. Fixed a missing await keyword and removed commented-out unreachable code to streamline execution paths.

--- src/webauthn.js
+++ src/webauthn.js
@@ -1,122 +1,169 @@
-const { generateRegistrationOptions, generateAuthenticationOptions, verifyRegistrationResponse, verifyAuthenticationResponse } = require('@simplewebauthn/server');
-const User = require('./models/User');
+const {
+  generateRegistrationOptions,
+  generateAuthenticationOptions,
+  verifyRegistrationResponse,
+  verifyAuthenticationResponse,
+} = require("@simplewebauthn/server");
+const User = require("./models/User");
+const { Binary } = require("mongodb");
 
-const rpID = 'localhost'; // Replace with your domain
-const rpName = 'Localhost Dev'; // Replace with your organization name
+const rpID = "localhost"; // Replace with your domain
+const rpName = "Localhost Dev"; // Replace with your organization name
 
-async function getRegistrationOptions({user, authenticatorSelection}) {
-    console.log("webauthn/getRegistrationOptions/user:", user);
-    console.log("webauthn/getRegistrationOptions/authenticatorSelection:", authenticatorSelection);
-    const options = generateRegistrationOptions({
-        rpName: rpName,
-        rpID: rpID,
-        userID: user._id.toString(),
-        userName: user.email,
-        userDisplayName: `${user.firstName} ${user.lastName}`, // Add user's display name
-        authenticatorSelection: authenticatorSelection,
-        attestation: 'direct', // or 'indirect', 'none'
-        excludeCredentials: user.authenticators.map(auth => ({
-            id: auth.credentialID,
-            type: 'public-key',
-            transports: ['usb', 'nfc', 'ble'], // Specify allowed transports
-        })),
-    });
+async function getRegistrationOptions({ user, authenticatorSelection }) {
+  console.log("webauthn/getRegistrationOptions/user:", user);
+  console.log(
+    "webauthn/getRegistrationOptions/authenticatorSelection:",
+    authenticatorSelection
+  );
+  const options = generateRegistrationOptions({
+    rpName: rpName,
+    rpID: rpID,
+    userID: user._id.toString(),
+    userName: user.email,
+    userDisplayName: `${user.firstName} ${user.lastName}`, // Add user's display name
+    authenticatorSelection: authenticatorSelection,
+    attestation: "direct", // or 'indirect', 'none'
+    excludeCredentials: user.authenticators.map((auth) => ({
+      id: auth.credentialID,
+      type: "public-key",
+    })),
+  });
 
-    return options;
+  return options;
 }
 
 async function verifyRegistration(user, response, storedChallenge) {
-    try {
-        const verification = await verifyRegistrationResponse({
-            response: response,
-            expectedChallenge: storedChallenge,
-            expectedOrigin: `https://${rpID}`,
-            expectedRPID: rpID,
-        });
+  try {
+    const verification = await verifyRegistrationResponse({
+      response: response,
+      expectedChallenge: storedChallenge,
+      expectedOrigin: `https://${rpID}`,
+      expectedRPID: rpID,
+    });
 
-        // console.log("webauthn/verifyRegistration/verification: ", verification);
+    // console.log("webauthn/verifyRegistration/verification: ", verification);
 
-        if (verification.verified) {
-            // Convert Uint8Array to Buffer
-            const credentialIDBuffer = Buffer.from(verification.registrationInfo.credentialID);
-            const credentialPublicKeyBuffer = Buffer.from(verification.registrationInfo.credentialPublicKey);
+    if (verification.verified) {
+      // Convert Uint8Array to Buffer
+      const credentialIDBuffer = Buffer.from(
+        verification.registrationInfo.credentialID
+      );
+      const credentialPublicKeyBuffer = Buffer.from(
+        verification.registrationInfo.credentialPublicKey
+      );
 
-            user.authenticators.push({
-                credentialID: credentialIDBuffer,
-                credentialPublicKey: credentialPublicKeyBuffer,
-                counter: verification.registrationInfo.counter,
-            });
-            await user.save();
-        }
-
-        return verification.verified;
-    } catch (error) {
-        console.error("Error in verifyRegistration:", error);
-        return false;
+      user.authenticators.push({
+        credentialID: credentialIDBuffer,
+        credentialPublicKey: credentialPublicKeyBuffer,
+        counter: verification.registrationInfo.counter,
+      });
+      await user.save();
     }
+
+    return verification.verified;
+  } catch (error) {
+    console.error("Error in verifyRegistration:", error);
+    return false;
+  }
 }
 
 // Function to get authentication options
-// async function getAuthenticationOptions(userId) {
-//     const user = await User.findById(userId);
-//     const authenticators = user.authenticators.map(auth => ({
-//         credentialID: auth.credentialID,
-//     }));
-
-//     const options = generateAuthenticationOptions({
-//         allowCredentials: authenticators,
-//         userVerification: 'preferred',
-//     });
-
-//     return options;
-// }
-
-// Function to get authentication options
 async function getAuthenticationOptions(userId) {
-    const user = await User.findById(userId);
-    const options = generateAuthenticationOptions({
-        rpID,
-        allowCredentials: user.authenticators.map(auth => ({
-            id: auth.credentialID,
-            type: 'public-key',
-            transports: ['internal']
-        })),
-        userVerification: 'preferred',
-    });
-
-    // Store the challenge in the database
-    user.currentChallenge = options.challenge;
+  const user = await User.findById(userId);
+  const options = await generateAuthenticationOptions({
+    rpID,
+    allowCredentials: user.authenticators.map((auth) => ({
+      id: auth.credentialID,
+      type: "public-key",
+      transports: ["internal"],
+    })),
+    userVerification: "preferred",
+  });
+  console.log(
+    "webauthn/getAuthenticationOptions/options.challenge:",
+    options.challenge
+  );
+  // Store the challenge in the database
+  user.currentChallenge = options.challenge;
+  console.log(
+    "webauthn/getAuthenticationOptions/user.currentChallenge:",
+    user.currentChallenge
+  );
+  try {
     await user.save();
+  } catch (error) {
+    console.error(
+      "webauthn/getAuthenticationOptions Error saving user:",
+      error
+    );
+    console.error("webauthn/getAuthenticationOptions user:", user);
+  }
 
-    return options;
+  return options;
 }
 
 // Function to verify authentication
 async function verifyAuthentication(userId, response) {
-    const user = await User.findById(userId);
-    // Retrieve the stored challenge from the database
-    const storedChallenge = user.currentChallenge;
+  const user = await User.findById(userId);
+  console.log("webauthn/verifyAuthentication/user:", user);
+  // Retrieve the stored challenge from the database
+  const storedChallenge = user.currentChallenge;
+  console.log(
+    "webauthn/verifyAuthentication/storedChallenge:",
+    storedChallenge
+  );
+  console.log("webauthn/verifyAuthentication/response:", response);
+  let responseIdBase64 = response.id.replace(/-/g, "+").replace(/_/g, "/");
+  let responseIdBinary = Binary.createFromBase64(responseIdBase64, 0);
+  let expectedAuthenticator = await user.authenticators.find((auth) =>
+    auth.credentialID.equals(responseIdBinary)
+  );
+  if (!expectedAuthenticator) {
+    throw new Error(
+      `Could not find authenticator for response ID${response.id} for auth ID ${auth.credentialID}`
+    );
+  }
+  console.log(
+    "webauthn/verifyAuthentication/expectedAuthenticator:",
+    expectedAuthenticator
+  );
+  console.log("webauthn/verifyAuthentication/response.id:", response.id);
+  console.log(
+    "webauthn/verifyAuthentication/user.authenticators:",
+    user.authenticators
+  );
+  user.authenticators.forEach((auth) => {
+    console.log(
+      "webauthn/verifyAuthentication/auth.credentialID:",
+      auth.credentialID
+    );
+    console.log(
+      "webauthn/verifyAuthentication/auth.credentialID.equals(response.id):",
+      auth.credentialID.equals(response.id)
+    );
+  });
+  const verification = await verifyAuthenticationResponse({
+    response: response,
+    expectedChallenge: storedChallenge,
+    expectedOrigin: `https://${rpID}`,
+    expectedRPID: rpID,
+    expectedAuthenticator,
+  });
+  console.log("webauthn/verifyAuthentication/verification:", verification);
 
-    const verification = verifyAuthenticationResponse({
-        response: response,
-        expectedChallenge: storedChallenge,
-        expectedOrigin: `https://${rpID}`,
-        expectedRPID: rpID,
-        authenticator: user.authenticators.find(auth => auth.credentialID.equals(response.id)),
-    });
+  if (verification.verified) {
+    expectedAuthenticator.counter = verification.authenticationInfo.newCounter;
+    user.currentChallenge = null;
+    await user.save();
+  }
 
-    if (verification.verified) {
-        expectedAuthenticator.counter = verification.authenticationInfo.newCounter;
-        user.currentChallenge = null;
-        await user.save();
-    }
-
-    return verification.verified;
+  return verification.verified;
 }
 
 module.exports = {
-    getRegistrationOptions,
-    verifyRegistration,
-    getAuthenticationOptions,
-    verifyAuthentication,
+  getRegistrationOptions,
+  verifyRegistration,
+  getAuthenticationOptions,
+  verifyAuthentication,
 };
